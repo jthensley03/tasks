a100c4e6f854733d7514c494b8b69b2854a735f7
jasonhensley <jasonh@udel.edu>
2022-10-05 14:44:24 -0400

    merge

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -10,6 +10,8 @@ export function RevealAnswer(): JSX.Element {
         <div>
             <Button onClick={flipVisibility}>Reveal Answer</Button>
             {visible && <div>42</div>}
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {visible && <div>42</div>}
         </div>
     );
 }
